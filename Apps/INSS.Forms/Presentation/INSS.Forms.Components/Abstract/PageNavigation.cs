07eef2a76023361efc630e98268b8fe1c31ff7ae
David Cook <davescook@outlook.com>
2025-08-07 18:33:24 +0100

    Rename components, fix form attributes.

---
+++ Apps/INSS.Forms/Presentation/INSS.Forms.Components/Abstract/PageNavigation.cs
@@ -0,0 +1,51 @@
+﻿//using Microsoft.AspNetCore.Components;
+
+//namespace INSS.Forms.Components.Abstract
+//{
+//    public class PageNavigation : ComponentBase
+//    {
+//        private NavigationItem navigationItem = new(0, []);
+
+//        //public bool IsVisible(string pageName)
+//        //{
+//        //    if (navigationItem.PageNames.Length > 0)
+//        //    {
+//        //        if(navigationItem.PageNames.Any(p => p == pageName))
+//        //        {
+//        //            return true;
+//        //        }
+//        //    }
+//        //    else
+//        //    {
+//        //        return false;
+//        //    }
+//        //}
+
+//        public void Next()
+//        {
+//            if (navigationItem.CurrentPageIndex < navigationItem.PageNames.Length - 1)
+//            {
+//                navigationItem.CurrentPageIndex++;
+//            }
+//        }
+
+//        public void Back()
+//        {
+//            if (navigationItem.CurrentPageIndex > 0)
+//            {
+//                navigationItem.CurrentPageIndex--;
+//            }
+//        }   
+
+//        struct NavigationItem
+//        {
+//            public int CurrentPageIndex { get; set; }
+//            public string[] PageNames { get; set; }
+//            public NavigationItem(int currentPageIndex, string[] pageNames)
+//            {
+//                CurrentPageIndex = currentPageIndex;
+//                PageNames = pageNames;
+//            }
+//        }
+//    }
+//}
