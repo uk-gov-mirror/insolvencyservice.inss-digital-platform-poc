d2a86e6a88ed8463148d2d6fb1e00d53f2cea6b4
David Cook <davescook@outlook.com>
2025-08-08 12:08:42 +0100

    Created a form pager.

---
+++ Apps/INSS.Forms/Presentation/INSS.Forms.Components/Abstract/FormPager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace INSS.Forms.Components.Abstract
+{
+    public abstract class FormPager : ComponentBase
+    {
+        public bool IsPageVisible(string pageName)
+        {
+            int index = Array.IndexOf(PageNames, pageName);
+            return index == currentPageIndex;
+        }
+
+        public EventCallback OnNext => EventCallback.Factory.Create(this, Next);
+
+        public async Task Next()
+        {
+            if (currentPageIndex < PageNames.Length - 1)
+            {
+                currentPageIndex++;
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public async Task Back()
+        {
+            if (currentPageIndex > 0)
+            {
+                currentPageIndex--;
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public string[] PageNames { get; set; } = [];
+
+        private int currentPageIndex = 0;
+    }
+}
