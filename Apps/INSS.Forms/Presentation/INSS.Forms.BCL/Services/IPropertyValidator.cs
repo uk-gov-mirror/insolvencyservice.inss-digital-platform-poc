3ec61ea67208d1ea10b0b385b9b0fe47d0a8e546
Dave Cook <Dave.Cook@insolvency.gov.uk>
2025-09-16 11:44:52 +0100

    Remove namespace

--- Apps/INSS.Forms/Presentation/INSS.Forms.BCL/Services/IPropertyValidator.cs
+++ Apps/INSS.Forms/Presentation/INSS.Forms.BCL/Services/IPropertyValidator.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace INSS.Forms.BCL.Services
 {
     public interface IPropertyValidator
     {
-        IList<System.ComponentModel.DataAnnotations.ValidationResult> ValidateProperties<T>(T model, IEnumerable<string> propertyNames);
-        IList<System.ComponentModel.DataAnnotations.ValidationResult> ValidateAllProperties<T>(T model);
+        IList<ValidationResult> ValidateProperties<T>(T model, IEnumerable<string> propertyNames);
+        IList<ValidationResult> ValidateAllProperties<T>(T model);
     }
 }
