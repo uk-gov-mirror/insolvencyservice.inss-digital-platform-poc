1a0ee2ad825b53141a654be1bdd825c9136ce78e
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2025-11-05 13:49:20 +0000

    Remove old poc code

--- Apps/INSS.Forms/INSS.Forms.Runner.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace INSS.Forms.Runner.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
