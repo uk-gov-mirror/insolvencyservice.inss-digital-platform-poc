cb316ed58cd269dd55f55597da62f530619afc15
Glen Stone <Glen.Stone@insolvency.gov.uk>
2025-11-11 11:55:20 +0000

    Updated Address component and added it to 'About You' section.

--- Apps/INSS.Formsv2/INSS.WebApp/INSS.Web.Components/Models/AddressModel.cs
+++ Apps/INSS.Formsv2/INSS.WebApp/INSS.Web.Components/Models/AddressModel.cs
@@ -21,5 +21,8 @@ public class AddressModel : PageModel
     public string? County { get; set; }
  
     [Required(ErrorMessage = "Enter postcode")]
+    [RegularExpression(
+        @"^(GIR ?0AA|(?:(?:[A-PR-UWYZa-pr-uwyz][0-9][0-9]?|[A-PR-UWYZa-pr-uwyz][A-HK-Ya-hk-y][0-9][0-9]?|[A-PR-UWYZa-pr-uwyz][0-9][A-HJKSTUWa-hjkstuw]?|[A-PR-UWYZa-pr-uwyz][A-HK-Ya-hk-y][0-9][ABEHMNPRVWXYabehmnprvwxy])) ?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2})$",
+        ErrorMessage = "Enter a full UK postcode")]
     public string Postcode { get; set; } = string.Empty;
 }
